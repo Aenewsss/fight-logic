7d416510a35bf5e657d3c5c542dfb2879097caa6
aenewsss <aenamartinelli@gmail.com>
2024-05-11 19:17:48 -0300

    v3 card benefits

--- src/components/ScheduleBoard.tsx
+++ src/components/ScheduleBoard.tsx
@@ -81,36 +81,36 @@ export default function ScheduleBoard() {
         </div>
     )
     else return (
-        <div className="bg-black py-8 px-16 text-white flex flex-col gap-8 mb-10 items-center" style={{ boxShadow: '-2px -2px 10px 0 rgb(0,0,0), 2px 2px 10px 0 rgb(0,0,0)' }}>
+        <div className="bg-black py-8 lg:px-16 text-white flex flex-col gap-8 mb-10 items-center" style={{ boxShadow: '-2px -2px 10px 0 rgb(0,0,0), 2px 2px 10px 0 rgb(0,0,0)' }}>
             <ul className="flex  list-none text-2xl font-questrial">
-                <li className="md:min-w-[140px] min-w-[60px] text-center">Domingo</li>
-                <li className="md:min-w-[140px] min-w-[60px] text-center">Segunda</li>
-                <li className="md:min-w-[140px] min-w-[60px] text-center">Terça</li>
-                <li className="md:min-w-[140px] min-w-[60px] text-center">Quarta</li>
-                <li className="md:min-w-[140px] min-w-[60px] text-center">Quinta</li>
-                <li className="md:min-w-[140px] min-w-[60px] text-center">Sexta</li>
-                <li className="md:min-w-[140px] min-w-[60px] text-center">Sábado</li>
+                <li className="lg:min-w-[140px] md:min-w-[110px] min-w-[60px] text-center">Domingo</li>
+                <li className="lg:min-w-[140px] md:min-w-[110px] min-w-[60px] text-center">Segunda</li>
+                <li className="lg:min-w-[140px] md:min-w-[110px] min-w-[60px] text-center">Terça</li>
+                <li className="lg:min-w-[140px] md:min-w-[110px] min-w-[60px] text-center">Quarta</li>
+                <li className="lg:min-w-[140px] md:min-w-[110px] min-w-[60px] text-center">Quinta</li>
+                <li className="lg:min-w-[140px] md:min-w-[110px] min-w-[60px] text-center">Sexta</li>
+                <li className="lg:min-w-[140px] md:min-w-[110px] min-w-[60px] text-center">Sábado</li>
             </ul>
             <div className="flex min-h-[400px]">
-                <ul className="md:min-w-[140px] min-w-[60px] border-r border-[#8A8A8A] font-inter">
+                <ul className="lg:min-w-[140px] md:min-w-[110px] min-w-[60px] border-r border-[#8A8A8A] font-inter">
                     {Object.keys(schedule.sunday).map((el, index) => <li className="text-center" key={index}>{el}h</li>)}
                 </ul>
-                <ul className="md:min-w-[140px] min-w-[60px] border-r border-[#8A8A8A]">
+                <ul className="lg:min-w-[140px] md:min-w-[110px] min-w-[60px] border-r border-[#8A8A8A]">
                     {Object.keys(schedule.monday).map((el, index) => <li className="text-center" key={index}>{el}h</li>)}
                 </ul>
-                <ul className="md:min-w-[140px] min-w-[60px] border-r border-[#8A8A8A]">
+                <ul className="lg:min-w-[140px] md:min-w-[110px] min-w-[60px] border-r border-[#8A8A8A]">
                     {Object.keys(schedule.tuesday).map((el, index) => <li className="text-center" key={index}>{el}h</li>)}
                 </ul>
-                <ul className="md:min-w-[140px] min-w-[60px] border-r border-[#8A8A8A]">
+                <ul className="lg:min-w-[140px] md:min-w-[110px] min-w-[60px] border-r border-[#8A8A8A]">
                     {Object.keys(schedule.wednesday).map((el, index) => <li className="text-center" key={index}>{el}h</li>)}
                 </ul>
-                <ul className="md:min-w-[140px] min-w-[60px] border-r border-[#8A8A8A]">
+                <ul className="lg:min-w-[140px] md:min-w-[110px] min-w-[60px] border-r border-[#8A8A8A]">
                     {Object.keys(schedule.thursday).map((el, index) => <li className="text-center" key={index}>{el}h</li>)}
                 </ul>
-                <ul className="md:min-w-[140px] min-w-[60px] border-r border-[#8A8A8A]">
+                <ul className="lg:min-w-[140px] md:min-w-[110px] min-w-[60px] border-r border-[#8A8A8A]">
                     {Object.keys(schedule.friday).map((el, index) => <li className="text-center" key={index}>{el}h</li>)}
                 </ul>
-                <ul className="md:min-w-[140px] min-w-[60px]">
+                <ul className="lg:min-w-[140px] md:min-w-[110px] min-w-[60px]">
                     {Object.keys(schedule.saturday).map((el, index) => <li className="text-center" key={index}>{el}h</li>)}
                 </ul>
             </div>
--- src/components/benefits-card.tsx
+++ src/components/benefits-card.tsx
@@ -10,16 +10,16 @@ export default function BenefitsCard() {
     const [opened, setOpened] = useState(false);
 
     if (!pathname.includes('admin') && !pathname.includes('login') && !pathname.includes('cadastro')) return (
-        <div onClick={_ => setOpened(!opened)} className={`bg-[#ffd600] shadow-lg shadow-[#ffd600] fixed top-[25%] h-[200px] rounded-md transition-all cursor-pointer p-4 z-10 ${opened ? 'w-[260px]' : 'md:w-11 w-6'}`}>
+        <div onClick={_ => setOpened(!opened)} className={`bg-[#ffd600] shadow-lg shadow-[#ffd600] fixed md:top-[25%] md:h-[200px] top-10 h-10 rounded-md transition-all cursor-pointer p-4 z-10 ${opened ? 'w-[260px]' : 'md:w-11 w-6'}`}>
             {opened ?
                 <div className="flex flex-col gap-8 justify-center h-full">
-                    <h3 className="font-inter font-semibold text-2xl">Garanta seus benefícios</h3>
-                    <Link href='/#fundador' className="bg-white px-2 py-3 font-inter rounded-xl relative text-center z-10">
+                    <h3 className="font-inter font-semibold text-2xl md:block hidden">Garanta seus benefícios</h3>
+                    <Link href='/#fundador' className="bg-white md:px-2 md:py-3 font-inter rounded-xl relative text-center z-10">
                         Seja um membro fundador
                         <Image className="absolute -top-[8px] -right-[8px]" src="/icons/crown.svg" width={20} height={20} alt="Ícone coroa" />
                     </Link>
                 </div>
-                : <ul className="list-none font-inter font-semibold text-center text-sm md:ml-0 -ml-1">
+                : <ul className="list-none font-inter font-semibold text-center text-sm md:ml-0 -ml-1 md:block hidden">
                     <li>B</li>
                     <li className="-mt-1">E</li>
                     <li className="-mt-1">N</li>
@@ -31,9 +31,9 @@ export default function BenefitsCard() {
                     <li className="-mt-1">O</li>
                     <li className="-mt-1">S</li>
                 </ul>}
-            {!opened && <Image className="absolute -top-[8px] -right-[8px]" src="/icons/crown.svg" width={20} height={20} alt="Ícone coroa" />}
+            {!opened && <Image className="absolute md:-top-[8px] md:-right-[8px] md:ml-0 -ml-2 md:mt-0 -mt-2" src="/icons/crown.svg" width={20} height={20} alt="Ícone coroa" />}
             <div className={`h-full ${opened ? 'w-[240px]' : 'md:w-8 w-4'} absolute top-0 flex items-center`}>
-                <Image className={`absolute right-0 ${opened && 'rotate-180'}`} src="/icons/arrow-right-card.svg" width={8} height={8} alt="Ícone seta para direita" />
+                <Image className={`absolute right-0 ${opened && 'rotate-180'} md:block hidden`} src="/icons/arrow-right-card.svg" width={8} height={8} alt="Ícone seta para direita" />
             </div>
         </div>
     )
