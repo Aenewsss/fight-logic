b27b267561192f27b667725e8897efd0d9f069dd
aenewsss <aenamartinelli@gmail.com>
2024-05-11 17:23:08 -0300

    some small fixes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,7 +7,10 @@ import { useEffect, useState } from "react"
 export default function Navbar() {
     const pathname = usePathname()
 
-    const [navClass, setNavClass] = useState<{ marginTop: string, bgColor: string, textColor: string, container:boolean }>(
+    const [navClass, setNavClass] = useState<{
+        marginTop: string, bgColor: string,
+        textColor: string, container: boolean,
+    }>(
         { marginTop: ' mt-4', bgColor: 'bg-white', textColor: 'text-black', container: true }
     );
 
@@ -16,8 +19,8 @@ export default function Navbar() {
         function scrollListener() {
             const scrollPos = Math.abs(document.body.getBoundingClientRect().top)
 
-            if (scrollPos > 10) setNavClass({ marginTop: ' mt-0', bgColor: 'bg-black', textColor: 'text-white',container: false})
-            else setNavClass({ marginTop: ' mt-4', bgColor: 'bg-white', textColor: 'text-black',container: true })
+            if (scrollPos > 10) setNavClass({ marginTop: ' mt-0', bgColor: 'bg-black', textColor: 'text-white', container: false, })
+            else setNavClass({ marginTop: ' mt-4', bgColor: 'bg-white', textColor: 'text-black', container: true })
         }
 
         window.addEventListener('scroll', scrollListener)
@@ -31,17 +34,17 @@ export default function Navbar() {
         <nav className={`flex justify-center fixed w-full z-20 ${navClass.marginTop} transition-all `}>
             <div className={`${navClass.container && 'container'} ${!navClass.container && 'w-full'} ${navClass.bgColor} ${navClass.textColor} flex transition-all  justify-around shadow-md ${navClass.marginTop == 'mt-4' && 'shadow-gray-400'} items-center font-questrial`}>
                 <ul className="list-none flex gap-4">
-                    <li><Link href="/horarios">Nossos horários</Link></li>
-                    <li><Link href="/equipes-parceiras">Equipes parceiras</Link></li>
-                    <li><Link href="/#diferenciais">Diferenciais Fight Logic</Link></li>
+                    <li className="hover:scale-105 transition-all"><Link href="/horarios">Nossos horários</Link></li>
+                    <li className="hover:scale-105 transition-all"><Link href="/equipes-parceiras">Equipes parceiras</Link></li>
+                    <li className="hover:scale-105 transition-all"><Link href="/#diferenciais">Diferenciais Fight Logic</Link></li>
                 </ul>
                 <Link href="/">
                     <Image src={`${navClass.container ? '/logo-amarela-dark.png' : '/logo-branca.png'}`} width={navClass.container ? 106 : 76} height={38} alt="Logo Fight Logic" />
                 </Link>
                 <ul className="list-none flex gap-4">
-                    <li className="after:bg-amber-400 after:absolute after:w-full after:h-[2px] after:rounded after:drop-shadow-lg  after:-bottom-0 after:left-0 after:shadow-md after:shadow-amber-500 after:animate-pulse relative"><Link href="/matricula">Faça parte do time</Link></li>
-                    <li><Link href="/#contato">Entre em contato</Link></li>
-                    <li><Link href="/quem-somos">Quem somos</Link></li>
+                    <li className="after:bg-amber-400 hover:scale-105 transition-all after:absolute after:w-full after:h-[2px] after:rounded after:drop-shadow-lg  after:-bottom-0 after:left-0 after:shadow-md after:shadow-amber-500 after:animate-pulse relative"><Link href="/matricula">Faça parte do time</Link></li>
+                    <li className="hover:scale-105 transition-all"><Link href="/#contato">Entre em contato</Link></li>
+                    <li className="hover:scale-105 transition-all"><Link href="/quem-somos">Quem somos</Link></li>
                 </ul>
             </div>
         </nav>
--- src/components/whatsapp-button.tsx
+++ src/components/whatsapp-button.tsx
@@ -7,7 +7,7 @@ export default function WhatsAppButton() {
     const pathname = usePathname()
 
     if (!pathname.includes('admin')) return (
-        <Link className="fixed bottom-5 right-5 transition-all hover:scale-105" href="https://api.whatsapp.com/send?phone=5561992634979&amp;text=Olá, venho através do site e gostaria de saber mais sobre os serviços da Fight Logic.">
+        <Link target="_blank" className="fixed bottom-5 right-5 transition-all hover:scale-105" href="https://api.whatsapp.com/send?phone=5561992634979&amp;text=Olá, venho através do site e gostaria de saber mais sobre os serviços da Fight Logic.">
             <Image className="shadow-black drop-shadow-xl" src="/icons/whatsapp.svg" height={60} width={60} alt="Ícone do WhatsApp"/>
         </Link>
     )
