dd494c6cbb6dfcebb6eaf0cff8aa11cfa936f5f4
aenewsss <aenamartinelli@gmail.com>
2024-05-13 20:52:37 -0300

    cta inserted on page top

--- src/sections/top.tsx
+++ src/sections/top.tsx
@@ -1,6 +1,7 @@
 'use client'
 import bannerService from "@/services/banner.service";
 import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Top() {
@@ -23,11 +24,14 @@ export default function Top() {
         <section className="relative z-0 flex justify-center items-center">
             <Image unoptimized className="object-cover w-full h-screen" src={banner} width={300} height={100} alt="Banner" />
             <div className="absolute bg-black opacity-60 w-full h-full"></div>
-            <h1 className="absolute md:text-6xl text-4xl container md:text-center text-white font-bold font-questrial md:leading-[80px] leading-[50px] md:px-auto px-4">
-                Domine cada movimento.<br />
-                Supere os desafios.<br />
-                Sua jornada começa aqui.
-            </h1>
+            <div className="absolute flex flex-col  md:px-auto px-4 gap-4">
+                <h1 className=" md:text-6xl text-4xl container md:text-center text-white font-bold font-questrial md:leading-[80px] leading-[50px]">
+                    Domine cada movimento.<br />
+                    Supere os desafios.<br />
+                    Sua jornada começa aqui.
+                </h1>
+                <Link href="/matricula" className="bg-white flex md:self-center self-start text-black px-8 py-1 font-questrial text-lg rounded-md transition-all hover:scale-105">Matricular agora</Link>
+            </div>
         </section>
     )
 }
