b7e9e381f8d1c50f27772085751dc7bfad772b1d
aenewsss <aenamartinelli@gmail.com>
2024-05-15 16:13:40 -0300

    fixing contact form

--- src/app/actions/contact.ts
+++ src/app/actions/contact.ts
@@ -1,5 +1,5 @@
 export async function contact(state: any, form: FormData) {
-    return (await fetch('/api/contact', {
+    return (await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/contact/contact-form`, {
         method: 'POST',
         body: form
     })).json()
--- src/app/api/contact/contact-form/route.ts
+++ src/app/api/contact/contact-form/route.ts
@@ -17,31 +17,27 @@ export async function POST(request: NextRequest) {
                 pass: process.env.NEXT_PUBLIC_NODEMAILER_PASS,
             }
         });
+        const formData = await request.formData()
 
-        const body = await request.json()
-
-
-        const { email, name, subscription: subscriptionId } = body.data.object.customer_details
-
-        const subscription = (await stripe.subscriptions.list()).data.find(el => el.id == subscriptionId)
-        const { product: productId, id: priceId } = subscription.items.data[0].plan
-
-        const { name: productName } = (await stripe.products.list()).data.find(el => el.id == productId)
-
+        const name = formData.get('name')
+        const phone = formData.get('phone')
+        const email = formData.get('email')
+        const message = formData.get('message')
+        
         const mail = await transporter.sendMail({
             from: process.env.NEXT_PUBLIC_NODEMAILER_EMAIL,
-            to: email,
-            subject: `Matrícula realizada na Fight Logic`,
+            to: process.env.NEXT_PUBLIC_NODEMAILER_EMAIL,
+            replyTo: email,
+            subject: `Novo lead Site Fight Logic`,
             html: `
-                <p>${name}, PARABÉNS POR QUERER FAZER PARTE DESSE TIME!</p>
-                <p>Você acabou de realizar a matrícula na seguinte modalidade: ${productName}</p>
-            `,
+        <p>Cliente: ${name} </p>
+        <p>E-mail: ${email} </p>
+        <p>Telefone: ${phone} </p>
+        <p>Mensagem: ${message} </p>
+        `,
         })
 
         return NextResponse.json({ data: mail, error: null })
-
-
-
     } catch (error) {
         return NextResponse.json({ data: null, error: error.message })
     }
