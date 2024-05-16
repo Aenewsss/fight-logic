c557b8be93ae70df7ce77cba8032fd875936c32f
aenewsss <aenamartinelli@gmail.com>
2024-05-16 10:04:52 -0300

    v4 animation

--- src/sections/teams.tsx
+++ src/sections/teams.tsx
@@ -27,13 +27,17 @@ export default function Teams() {
             {
                 teams.map((el, index) => (
                     <div key={index} className="mt-10 flex md:flex-nowrap flex-wrap shadow-black shadow-md">
-                        <Reveal animation="opacity">
-                            <Image unoptimized className="md:w-1/2 w-full max-h-[400px] object-cover" src={el.image} width={400} height={300} alt={`Foto do time ${el.name}`} />
-                        </Reveal>
-                        <div className="md:w-1/2 bg-black flex items-center p-4 md:flex-nowrap flex-wrap justify-center gap-4 md:text-left text-center">
+                        <div className="md:w-1/2 w-full">
                             <Reveal animation="opacity">
-                                <Image className="object-cover bg-white rounded-md" unoptimized src={el.logo} width={172} height={172} alt={`Logo ${el.name}`} />
+                                <Image unoptimized className="w-full max-h-[400px] object-cover" src={el.image} width={400} height={300} alt={`Foto do time ${el.name}`} />
                             </Reveal>
+                        </div>
+                        <div className="md:w-1/2 bg-black flex items-center p-4 md:flex-nowrap flex-wrap justify-center gap-4 md:text-left text-center">
+                            <div className="max-w-[172px]">
+                                <Reveal animation="opacity">
+                                    <Image className="object-cover bg-white rounded-md w-full" unoptimized src={el.logo} width={172} height={172} alt={`Logo ${el.name}`} />
+                                </Reveal>
+                            </div>
                             <Reveal animation="opacity">
                                 <p className="text-white font-inter px-4">{el.text}</p>
                             </Reveal>
