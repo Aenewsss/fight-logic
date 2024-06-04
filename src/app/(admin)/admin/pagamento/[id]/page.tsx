6888e125532901b8436d541a1d0b280654df640a
aenewsss <aenamartinelli@gmail.com>
2024-06-04 11:36:51 -0300

    admin payment removed

--- src/app/(admin)/admin/pagamento/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import ProductForm from "./product-form";
-
-export default function Page({ params }: { params: { id: string } }) {
-    return <ProductForm id={params.id} />
-}
--- src/app/(admin)/admin/pagamento/[id]/product-button.tsx
+++
@@ -1,8 +0,0 @@
-import Button from "@/components/button"
-import { useFormStatus } from 'react-dom'
-
-export function ProductButton() {
-  const { pending } = useFormStatus()
-
-  return <Button className="px-10 py-2" backgroundColor="amber" color="white" type='submit' disabled={pending} text={pending ? 'Carregando...' : 'Salvar'} />
-}
